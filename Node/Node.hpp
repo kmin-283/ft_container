ce93e310216bc2e227094aef407b02df79ab67c9
Kyunghan Min <kmin@c3r3s3.42seoul.kr>
2020-10-22 16:22:44 +0900

    not yet fix node transfer

--- Node/Node.hpp
+++ Node/Node.hpp
@@ -6,7 +6,7 @@
 /*   By: kmin <kmin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 13:31:13 by kmin              #+#    #+#             */
-/*   Updated: 2020/10/22 15:56:59 by kmin             ###   ########.fr       */
+/*   Updated: 2020/10/22 16:22:16 by kmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ namespace ft
         void transfer(NodeBase * const __first, NodeBase * const __last)
         {
             __last->mPrev->mNext = this;
-            if (__first != __last->mPrev) // last는 next 또는 prev의 비교대상이 될 수 없음 Node이기 때문에
+            if (__first != __last->mPrev) // last는 next 또는 prev의 비교대상이 될 수 없음 Node가 될 수 있기 때문에 end()인 경우
             {
                 __last->mNext = __last; // end()의 경우 맨 처음 상태인 self->next = self;
                 __last->mPrev = __last; // self->prev = self로 만들어줌
